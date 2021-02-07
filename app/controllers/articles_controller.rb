6dcf3f15ccbf235652cde53aedc40ecfc004f05a
Martin Najjar <martin_najjar@yahoo.com>
2021-02-07 22:40:25 +0300

    Add the category input field to the form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,20 +5,33 @@ class ArticlesController < ApplicationController
 
   def new
     @author = Author.find(current_author.id)
+    @category = Category.new
     @article = Article.new
   end
 
   def create
     @author = Author.find(params[:author_id])
     @article = @author.articles.build(article_params)
+    # categories = params[:category].split(',');
+    # categories.each do |cat|
+      # @category = Category.new(name: 'cat');
+      # if @category.save
+        # @article.categories << @category
+        # @category.articles << @article
+      # end
+    # end
 
     if @article.save
       redirect_to root_path
     end
   end
 
+  def article_categories
+    categories.join(', ')
+  end
+
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :article_categories)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,14 @@ class Article < ApplicationRecord
   has_many :votes
   has_many :article_categories
   has_many :categories, through: :article_categories
+
+  def article_categories
+    categories.collect(&:name).join(', ')
+  end
+
+  def article_categories=(string)
+    categories_names = string.split(',').collect { |s| s.strip.downcase }.uniq
+    new_or_found_categories = categories_names.collect { |name| Category.find_or_create_by(name: name) }
+    self.categories = new_or_found_categories
+  end
 end
